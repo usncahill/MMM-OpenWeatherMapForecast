41f98e2e82b4af917d9b2d5b9153ff4f9d5b8327
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-09-24 23:35:06 +0200

    Remove needle, Remove outdated comments, Format

--- node_helper.js
+++ node_helper.js
@@ -1,8 +1,8 @@
-/*********************************
+/** *******************************
 
   Node Helper for MMM-OpenWeatherMapForecast.
 
-  This helper is responsible for the DarkSky-compatible data pull from OpenWeather.
+  This helper is responsible for the data pull from OpenWeather.
   At a minimum the API key, Latitude and Longitude parameters
   must be provided.  If any of these are missing, the request
   to OpenWeather will not be executed, and instead an error
@@ -13,36 +13,33 @@
     units - one of "metric", "imperial", or "" (blank)
     lang - Any of the languages OpenWeather supports, as listed here: https://openweathermap.org/api/one-call-api#multi
 
-  The DarkSky-compatible API request looks like this:
+  The API request looks like this:
 
     https://api.openweathermap.org/data/3.0/onecall?lat=LATITUDE&lon=LONGITUDE&units=XXX&lang=YY&appid=API_KEY
 
 *********************************/
 
 const Log = require("logger");
-var NodeHelper = require("node_helper");
-var needle = require("needle");
-var moment = require("moment");
+const NodeHelper = require("node_helper");
+const moment = require("moment");
 
 module.exports = NodeHelper.create({
 
-    start: function() {
-        Log.log("====================== Starting node_helper for module [" + this.name + "]");
+    start () {
+        Log.log(`Starting node_helper for ${this.name}`);
     },
 
-    socketNotificationReceived: function(notification, payload) {
+    async socketNotificationReceived (notification, payload) {
         if (notification === "OPENWEATHER_ONE_CALL_FORECAST_GET") {
-
-            var self = this;
+            const self = this;
 
             if (payload.apikey == null || payload.apikey == "") {
-                Log.log("[MMM-OpenWeatherMapForecast] " + moment().format("D-MMM-YY HH:mm") + " ** ERROR ** No API key configured. Get an API key at https://openweathermap.org/api/one-call-api");
+                Log.log(`[MMM-OpenWeatherMapForecast] ${moment().format("D-MMM-YY HH:mm")} ** ERROR ** No API key configured. Get an API key at https://openweathermap.org/api/one-call-api`);
             } else if (payload.latitude == null || payload.latitude == "" || payload.longitude == null || payload.longitude == "") {
-                Log.log("[MMM-OpenWeatherMapForecast] " + moment().format("D-MMM-YY HH:mm") + " ** ERROR ** Latitude and/or longitude not provided.");
+                Log.log(`[MMM-OpenWeatherMapForecast] ${moment().format("D-MMM-YY HH:mm")} ** ERROR ** Latitude and/or longitude not provided.`);
             } else {
-
-                //make request to OpenWeather onecall API
-                var url = payload.endpoint +
+                // make request to OpenWeather onecall API
+                const url = payload.endpoint +
                     "?appid=" + payload.apikey +
                     "&lat=" + payload.latitude +
                     "&lon=" + payload.longitude +
@@ -51,24 +48,19 @@ module.exports = NodeHelper.create({
                 // "&exclude=minutely"
 
                 // Log.log("[MMM-OpenWeatherMapForecast] Getting data: " + url);
-                needle.get(url, function(error, response, body) {
-
-                    if (!error && response.statusCode == 200) {
-
-                        //Good response
-                        var resp = body; //needle automagically parses the response as JSON
+                try {
+                    const response = await fetch(url);
+                    if (response.ok) {
+                        const resp = await response.json();
                         resp.instanceId = payload.instanceId;
                         self.sendSocketNotification("OPENWEATHER_ONE_CALL_FORECAST_DATA", resp);
-
                     } else {
-                        Log.log("[MMM-OpenWeatherMapForecast] " + moment().format("D-MMM-YY HH:mm") + " ** ERROR ** " + response.statusCode);
+                        Log.log(`[MMM-OpenWeatherMapForecast] ${moment().format("D-MMM-YY HH:mm")} ** ERROR ** ${response.status}`);
                     }
-
-                });
-
+                } catch (error) {
+                    Log.log(`[MMM-OpenWeatherMapForecast] ${moment().format("D-MMM-YY HH:mm")} ** ERROR ** ${error.message}`);
+                }
             }
         }
-    },
-
-
+    }
 });
